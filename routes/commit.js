00c19da00419382b1d7e4baaeeb24583ea3f9156
Vctor <makarovviktor3i@gmail.com>
2019-04-25 20:23:58 +0300

    Another one

--- routes/commit.js
+++ routes/commit.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const context = require('../data');
+const context = require('../data')();
 
 router.get('/', async (req, res, next) => {
     try {
--- routes/repo.js
+++ routes/repo.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const commitRouter = require('./commit');
-const context = require('../data');
+const context = require('../data')();
 
 router.get('/', async (req, res, next) => {
     try {
